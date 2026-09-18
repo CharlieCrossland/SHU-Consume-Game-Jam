76f4fd48898919eb1b719b80eb007ea10d6283ce
NicolasBR04 <79269389+NicolasBR04@users.noreply.github.com>
2026-09-18 12:21:52 +0100

    Test Commit

---
+++ Assets/Scripts/Nicolas/Spawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}
